d4ac075621447858495a59abc390963a1dd3e8da
Juan R. Linares Orihuela <122564427+JuanLinaresOrihuela@users.noreply.github.com>
2023-09-22 23:48:12 -0400

    updates

--- Wordle/KeyboardController.swift
+++ Wordle/KeyboardController.swift
@@ -30,7 +30,7 @@ class KeyboardController: NSObject,
     // Exercise 1: Return the correct number of items in a section
     // Tip: There's a helper method you can use located in this class
     // START YOUR CODE HERE
-    return 0
+    return keyboardRows[section].count
     // END YOUR CODE HERE
   }
 
@@ -41,9 +41,9 @@ class KeyboardController: NSObject,
     cell.configure(with: keyboardRows[indexPath.section][indexPath.row])
     // Exercise 4: Pass in the `didSelectString` closure to the KeyboardCell's corresponding property
     // START YOUR CODE HERE
-    // ...
-    // END YOUR CODE HERE
+    cell.didSelectString = didSelectString
     return cell
+    // END YOUR CODE HERE
   }
 
   // MARK: - Private Methods
